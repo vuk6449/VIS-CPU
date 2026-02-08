f918f616ce6a32d403b01d5c8e3745229c9b8ba8
vuk6449 <vukstojanovicbg14@gmail.com>
2026-02-08 21:26:21 +0100

    First commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:8.0 AS base
+USER $APP_UID
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+ARG BUILD_CONFIGURATION=Release
+WORKDIR /src
+COPY ["VukCPU.csproj", "./"]
+RUN dotnet restore "VukCPU.csproj"
+COPY . .
+WORKDIR "/src/"
+RUN dotnet build "./VukCPU.csproj" -c $BUILD_CONFIGURATION -o /app/build
+
+FROM build AS publish
+ARG BUILD_CONFIGURATION=Release
+RUN dotnet publish "./VukCPU.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "VukCPU.dll"]
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿namespace VukCPU
+{
+    internal class ViscpuMain
+    {
+        // Registers
+        public static string Ax = "0000";
+
+        private static void Main(string[] args)
+        {
+            string inputPath = args.Length > 0
+                ? args[0]
+                : Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "main.bin"));
+
+            if (args.Length < 1)
+            {
+                Warn("WARN: No input file specified. Defaulting to main.bin");
+                Console.WriteLine("Usage: VukCPU.exe <code>");
+            }
+
+            if (!File.Exists(inputPath))
+            {
+                Error($"ERR: {inputPath} not found.");
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey(true);
+                Environment.Exit(1);
+            }
+
+            string code = File.ReadAllText(inputPath);
+            Exec(code);
+        }
+
+        private static void Exec(string code)
+        {
+            if (string.IsNullOrEmpty(code))
+            {
+                Error("Invalid code: Empty string");
+                return;
+            }
+
+            Console.Clear();
+
+            char sc = ' ';
+            
+            string[] tokens = code.Split(sc, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                if (tokens[i] == "b8")
+                {
+                    string b1 = tokens[i + 1];
+                    string b2 = tokens[i + 2];
+
+                    string word = $"{b1}{b2}";
+                    Ax = word;
+                }
+            }
+        }
+
+        private static void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        
+        private static void Warn(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
