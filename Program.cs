641558f2df36f8a6b835fa263dbf582fd3d1db78
vuk6449 <vukstojanovicbg14@gmail.com>
2026-02-08 22:13:41 +0100

    Small update: Added PT and DP

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@
     internal class ViscpuMain
     {
         // Registers
-        public static string Ax = "0000";
+        public static string Ax = "0000"; // Classic register
+        public static string Bx = "0000"; // Classic register
+        public static string Dp = "0000"; // Fallback for when PT takes an invalid input (e.g. pt 46)
 
         private static void Main(string[] args)
         {
@@ -45,13 +47,40 @@
 
             for (int i = 0; i < tokens.Length; i++)
             {
-                if (tokens[i] == "b8")
+                if (tokens[i] == "ax")
                 {
                     string b1 = tokens[i + 1];
                     string b2 = tokens[i + 2];
 
-                    string word = $"{b1}{b2}";
+                    string word = $"{b1}{b2}".ToLower();
                     Ax = word;
+                    i += 2;
+                }
+                else if (tokens[i] == "bx")
+                {
+                    string b1 = tokens[i + 1];
+                    string b2 = tokens[i + 2];
+
+                    string word = $"{b1}{b2}".ToLower();
+                    Bx = word;
+                    i += 2;
+                }
+                else if (tokens[i] == "dp")
+                {
+                    string b1 = tokens[i + 1];
+                    string b2 = tokens[i + 2];
+
+                    string word = $"{b1}{b2}".ToLower();
+                    Dp = word;
+                    i += 2;
+                }
+                else if (tokens[i] == "pt" && i + 1 < tokens.Length)
+                {
+                    string op = tokens[i + 1].ToLower();
+                    if (op == "a2") Console.WriteLine(Ax);
+                    else if (op == "b2") Console.WriteLine(Bx);
+                    else Console.WriteLine(Dp);
+                    i++;
                 }
             }
         }
